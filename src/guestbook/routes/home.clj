323ab49861fd186d233f1f297fd33f26e3254ef0
Trevyn Loris <trevyn.loris@gmail.com>
2015-03-03 00:17:01 -0600

    Initial commit.

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -1,15 +1,38 @@
 (ns guestbook.routes.home
   (:require [guestbook.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
-            [clojure.java.io :as io]))
-
-(defn home-page []
-  (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+            [compojure.core :refer [defroutes GET POST]]
+            [ring.util.response :refer [redirect]]
+            [clojure.java.io :as io]
+            [guestbook.db.core :as db]
+            [bouncer.core :as b]
+            [bouncer.validators :as v]))
 
 (defn about-page []
   (layout/render "about.html"))
 
+(defn validate-message [params]
+  (first
+    (b/validate
+      params
+      :name v/required
+      :message [v/required [v/min-count 10]])))
+
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (-> (redirect "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message! (assoc params :timestamp (java.util.Date.)))
+      (redirect "/"))))
+
+(defn home-page [{:keys [flash]}]
+  (layout/render
+    "home.html"
+    (merge {:messages (db/get-messages)}
+           (select-keys flash [:name :message :errors]))
+    :name (:name flash)))
+
 (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" request (home-page request))
+  (POST "/" request (save-message! request))
   (GET "/about" [] (about-page)))
